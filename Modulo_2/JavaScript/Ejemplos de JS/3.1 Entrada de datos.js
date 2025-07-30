4b1278f1ebef14560219667cc74e650f682bf9f9
danielzespitia <daniel.z.espitia@gmal.com>
2025-07-30 11:00:10 -0400

    v3.0.1

--- Modulo_2/JavaScript/Ejemplos de JS/3.1 Entrada de datos.js
+++ Modulo_2/JavaScript/Ejemplos de JS/3.1 Entrada de datos.js
@@ -1,15 +1,18 @@
 let dato = prompt("Por favor, ingresa un dato:");
 console.log("Has ingresado:", dato);
+console.log(`Tipo de dato ingresado: ${dato}`);
 
 // Convertir el dato ingresado a un número
 let numero = Number(dato);
 if (!isNaN(numero)) {
     console.log("El dato convertido a número es:", numero);
 }
+
 // Verificar si el dato es un número
 if (isNaN(numero)) {
     console.log("El dato ingresado no es un número válido.");
 }
+
 // Convertir el dato ingresado a un booleano
 let booleano = dato.toLowerCase() === "true";
 console.log("El dato convertido a booleano es:", booleano); 
