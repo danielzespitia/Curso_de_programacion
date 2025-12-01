2060cd03d5743162f8210681638dcfb4cb302833
danielzespitia <daniel.z.espitia@gmal.com>
2025-12-01 13:50:59 -0400

    v08

--- Modulo_2/JavaScript/Ejemplos/3.1 Entrada de datos.js
+++ Modulo_2/JavaScript/Ejemplos/3.1 Entrada de datos.js
@@ -1,5 +1,19 @@
 let dato = prompt("Por favor, ingresa un dato:");
 console.log("Has ingresado:", dato);
 console.log(`Tipo de dato ingresado: ${dato}`);
-
 console.log("Tipo de dato ingresado:", typeof dato);
+
+let edad = Number(prompt("Por favor, ingresa un dato:"));
+console.log("Has ingresado:", edad);
+console.log(`Tipo de dato ingresado: ${edad}`);
+console.log("Tipo de dato ingresado:", typeof edad);
+
+let esAdulto = Boolean(prompt("Por favor, ingresa un dato:"));
+console.log("Has ingresado:", esAdulto);
+console.log(`Tipo de dato ingresado: ${esAdulto}`);
+console.log("Tipo de dato ingresado:", typeof esAdulto);
+
+let precio = parseFloat(prompt("Por favor, ingresa un dato:"));
+console.log("Has ingresado:", precio);
+console.log(`Tipo de dato ingresado: ${precio}`);
+console.log("Tipo de dato ingresado:", typeof precio);
